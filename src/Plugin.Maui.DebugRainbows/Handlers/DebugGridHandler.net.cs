ba171bad85edd501d9a38116af5e42c2e74a0da8
Steven Thewissen <github@thewissen.io>
2024-05-21 11:44:04 +0200

    Update DebugGridHandler.net.cs

--- src/Plugin.Maui.DebugRainbows/Handlers/DebugGridHandler.net.cs
+++ src/Plugin.Maui.DebugRainbows/Handlers/DebugGridHandler.net.cs
@@ -5,46 +5,13 @@ namespace Plugin.Maui.DebugRainbows.Handlers
 {
     public partial class DebugGridHandler
     {
-        protected override MauiDebugGrid CreatePlatformView() => new(VirtualView);
+        protected override MauiDebugGrid CreatePlatformView() => throw new NotImplementedException();
 
-        protected override void ConnectHandler(MauiDebugGrid platformView)
-        {
-           throw new NotImplementedException();
-        }
-
-        protected override void DisconnectHandler(MauiDebugGrid platformView)
-		{
-           throw new NotImplementedException();
-        }
-
-        public static void MapMajorGridLineInterval(DebugGridHandler handler, DebugGrid debugGrid)
-        {
-           throw new NotImplementedException();
-		}
-
-		public static void MapMajorGridLines(DebugGridHandler handler, DebugGrid debugGrid)
-		{
-           throw new NotImplementedException();
-		}
-
-		public static void MapMinorGridLines(DebugGridHandler handler, DebugGrid debugGrid)
-		{
-           throw new NotImplementedException();
-		}
-
-		public static void MapGridOrigin(DebugGridHandler handler, DebugGrid debugGrid)
-		{
-           throw new NotImplementedException();
-		}
-
-		public static void MapHorizontalItemSize(DebugGridHandler handler, DebugGrid debugGrid)
-		{
-           throw new NotImplementedException();
-		}
-
-		public static void MapVerticalItemSize(DebugGridHandler handler, DebugGrid debugGrid)
-		{
-           throw new NotImplementedException();
-		}
-	}
+        public static void MapMajorGridLineInterval(DebugGridHandler handler, DebugGrid debugGrid) => throw new NotImplementedException();
+	public static void MapMajorGridLines(DebugGridHandler handler, DebugGrid debugGrid) => throw new NotImplementedException();
+	public static void MapMinorGridLines(DebugGridHandler handler, DebugGrid debugGrid) => throw new NotImplementedException();
+	public static void MapGridOrigin(DebugGridHandler handler, DebugGrid debugGrid) => throw new NotImplementedException();
+	public static void MapHorizontalItemSize(DebugGridHandler handler, DebugGrid debugGrid) => throw new NotImplementedException();
+	public static void MapVerticalItemSize(DebugGridHandler handler, DebugGrid debugGrid) => throw new NotImplementedException();
+    }
 }
